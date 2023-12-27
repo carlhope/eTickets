2b90982c10bebec3d2add409c6770d4229a58836
Carl hope <84421131+carlhope@users.noreply.github.com>
2023-12-27 16:31:40 +0000

    update

--- eTickets/Data/Services/IMoviesService.cs
+++ eTickets/Data/Services/IMoviesService.cs
@@ -4,7 +4,7 @@ using eTickets.Models;
 
 namespace eTickets.Data.Services
 {
-    public interface IMoviesService:IEntityBaseRepository<Movie>
+    public interface IMoviesService : IEntityBaseRepository<Movie>
     {
         Task<Movie> GetMovieByIdAsync(int id);
         Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
