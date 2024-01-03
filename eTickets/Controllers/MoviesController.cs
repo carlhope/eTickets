cfd05e212e828477e698e0eaa2dd04e724fbdfa2
Carl hope <84421131+carlhope@users.noreply.github.com>
2024-01-03 13:37:13 +0000

    update

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -29,8 +29,10 @@ namespace eTickets.Controllers
             var allMovies = await _service.GetAllAsync(n => n.Cinema);
             if (!string.IsNullOrEmpty(searchString))
             {
-                var filteredResults = allMovies.Where(n=>n.Name.Contains(searchString)||n.Description.Contains(searchString)).ToList();
-                return View("Index",filteredResults);
+                //var filteredResults = allMovies.Where(n=>n.Name.ToLower().Contains(searchString.ToLower())||n.Description.ToLower().Contains(searchString.ToLower())).ToList();
+                var filteredResultNew = allMovies.Where(n => string.Equals(n.Name, searchString, StringComparison.CurrentCultureIgnoreCase) || string.Equals(n.Description, searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
+
+                return View("Index",filteredResultNew);
             }
             return View("Index", allMovies);
         }
